3ce7b99bc1803c78cfb56b8a84da48616df5b061
munemahmadchowdhury <141803747+munemahmadchowdhury@users.noreply.github.com>
2026-02-16 17:30:13 +0000

    Deploying to gh-pages from @ munemahmadchowdhury/munemahmadchowdhury.github.io@4749338daf49f67d29889f2fc279e9dda3e2b85e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please click on the **PDF icon** to download the **CV**.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
