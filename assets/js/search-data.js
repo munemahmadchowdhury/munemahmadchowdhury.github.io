7015be24da77b9d04119143cda8c676a0368e1bd
munemahmadchowdhury <141803747+munemahmadchowdhury@users.noreply.github.com>
2026-05-08 16:25:58 +0000

    Deploying to gh-pages from @ munemahmadchowdhury/munemahmadchowdhury.github.io@b527e59bc644f58e35ea8641da881713cced6b97 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,7 +451,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "State of Accountability of State-Owned Enterprises in the Maldives for TI Maldives.",
           section: "Projects",handler: () => {
