8bb0fefc14a33d9e703707aeb1db5cc7a41c4fa5
munemahmadchowdhury <141803747+munemahmadchowdhury@users.noreply.github.com>
2026-05-08 15:09:35 +0000

    Deploying to gh-pages from @ munemahmadchowdhury/munemahmadchowdhury.github.io@a7d66b0d5db0df1f653b44fa2eafed6f2c73b5ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -458,7 +458,7 @@ ninja.data = [{
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          description: "Integrity Assessment of Bangladesh Wash Sector (Water Resources Management, Water Supply, Sanitation, and Climate Change Adaptation).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
