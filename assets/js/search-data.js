0fe197142c6cbbecc99d91100988ec44438dbe2c
munemahmadchowdhury <141803747+munemahmadchowdhury@users.noreply.github.com>
2026-05-10 07:19:40 +0000

    Deploying to gh-pages from @ munemahmadchowdhury/munemahmadchowdhury.github.io@48a47f961ac6c1d12b2269d8c8874c7561eaf1b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-blog-amp-code",
+          title: "blog &amp; code",
           description: "",
           section: "Navigation",
           handler: () => {
